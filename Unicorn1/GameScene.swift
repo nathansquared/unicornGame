ca860e574fb4555bba86b2b2fd8ffebc9f7d6b11
Nathan Adkins <nathanoadkins@gmail.com>
2016-05-10 10:04:26 -0700

    first save

--- Unicorn1/GameScene.swift
+++ Unicorn1/GameScene.swift
@@ -8,37 +8,111 @@
 
 import SpriteKit
 
+
+struct PhysicsCategory {
+    
+    static let Unicorn : UInt32 = 0x1 << 1
+    static let Ground : UInt32 = 0x1 << 2
+    static let Cloud : UInt32 = 0x1 << 3
+    
+}
+
+
 class GameScene: SKScene {
+    
+    var Ground = SKSpriteNode()
+    var Unicorn = SKSpriteNode()
+    var tapLocation = CGPoint(x: 0, y: 0)
+    
     override func didMoveToView(view: SKView) {
-        /* Setup your scene here */
-        let myLabel = SKLabelNode(fontNamed:"Chalkduster")
-        myLabel.text = "Hello, World!"
-        myLabel.fontSize = 45
-        myLabel.position = CGPoint(x:CGRectGetMidX(self.frame), y:CGRectGetMidY(self.frame))
+     
+        Ground = SKSpriteNode(imageNamed: "ground")
+        Ground.setScale(0.5)
+        Ground.position = CGPoint(x: self.frame.width / 2, y: 0 + Ground.frame.height / 4.5)
+        self.addChild(Ground)
         
-        self.addChild(myLabel)
+        Ground.physicsBody = SKPhysicsBody(rectangleOfSize: Ground.size)
+        Ground.physicsBody?.categoryBitMask = PhysicsCategory.Ground
+        Ground.physicsBody?.collisionBitMask = PhysicsCategory.Unicorn
+        Ground.physicsBody?.contactTestBitMask = PhysicsCategory.Unicorn
+        Ground.physicsBody?.affectedByGravity = false
+        Ground.physicsBody?.dynamic = false
+        Ground.zPosition = 3
+        
+        Unicorn = SKSpriteNode(imageNamed: "Unicorn1")
+        Unicorn.size = CGSize(width: 90, height: 105)
+        Unicorn.position = CGPoint(x: self.frame.width / 2, y: self.frame.height / 2)
+        self.addChild(Unicorn)
+        
+        Unicorn.physicsBody = SKPhysicsBody(circleOfRadius: Unicorn.frame.height / 2)
+        Unicorn.physicsBody?.categoryBitMask = PhysicsCategory.Unicorn
+        Unicorn.physicsBody?.collisionBitMask = PhysicsCategory.Ground | PhysicsCategory.Cloud
+        Unicorn.physicsBody?.contactTestBitMask = PhysicsCategory.Ground | PhysicsCategory.Cloud
+        Unicorn.physicsBody?.allowsRotation = false
+        Unicorn.physicsBody?.affectedByGravity = true
+        Unicorn.physicsBody?.dynamic = true
+        Unicorn.zPosition = 2
+        
+        createClouds()
+        
+        
+        
     }
     
     override func touchesBegan(touches: Set<UITouch>, withEvent event: UIEvent?) {
-       /* Called when a touch begins */
+
+        let touch : UITouch = touches.first! as UITouch
+        tapLocation = touch.locationInView(self.view)
         
-        for touch in touches {
-            let location = touch.locationInNode(self)
-            
-            let sprite = SKSpriteNode(imageNamed:"Spaceship")
-            
-            sprite.xScale = 0.5
-            sprite.yScale = 0.5
-            sprite.position = location
-            
-            let action = SKAction.rotateByAngle(CGFloat(M_PI), duration:1)
-            
-            sprite.runAction(SKAction.repeatActionForever(action))
-            
-            self.addChild(sprite)
-        }
+        Unicorn.physicsBody?.velocity = CGVectorMake(0, 0)
+        Unicorn.physicsBody?.applyImpulse(CGVectorMake((tapLocation.x - self.frame.width/5)/12, 150))
+        
+        
+    
     }
-   
+    
+ 
+    
+    func createClouds() {
+        
+        let cloudPair = SKNode()
+        
+        let leftCloud = SKSpriteNode(imageNamed: "CloudLeft")
+        let rightCloud = SKSpriteNode(imageNamed: "CloudRight")
+        
+        leftCloud.position = CGPoint(x: self.frame.width / 2 - 150, y: self.frame.height / 2 - 50)
+        rightCloud.position = CGPoint(x: self.frame.width / 2 + 150, y: self.frame.height / 2 + 50)
+        
+        leftCloud.setScale(0.07)
+        rightCloud.setScale(0.07)
+        
+        leftCloud.physicsBody = SKPhysicsBody(rectangleOfSize: rightCloud.size)
+        leftCloud.physicsBody?.categoryBitMask = PhysicsCategory.Cloud
+        leftCloud.physicsBody?.collisionBitMask = PhysicsCategory.Unicorn
+        leftCloud.physicsBody?.contactTestBitMask = PhysicsCategory.Unicorn
+        leftCloud.physicsBody?.dynamic = false
+        leftCloud.physicsBody?.affectedByGravity = false
+        
+        rightCloud.physicsBody = SKPhysicsBody(rectangleOfSize: leftCloud.size)
+        rightCloud.physicsBody?.categoryBitMask = PhysicsCategory.Cloud
+        rightCloud.physicsBody?.collisionBitMask = PhysicsCategory.Unicorn
+        rightCloud.physicsBody?.contactTestBitMask = PhysicsCategory.Unicorn
+        rightCloud.physicsBody?.dynamic = false
+        rightCloud.physicsBody?.affectedByGravity = false
+        
+        
+        cloudPair.addChild(leftCloud)
+        cloudPair.addChild(rightCloud)
+        
+        cloudPair.zPosition = 1
+        
+        self.addChild(cloudPair)
+        
+    }
+    
+    
+
+    
     override func update(currentTime: CFTimeInterval) {
         /* Called before each frame is rendered */
     }
